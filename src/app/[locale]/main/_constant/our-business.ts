f1536b9e666456130f077faadf8d7a84bea791f8
Jung Hong <junghong@ninewatt.com>
2025-11-26 10:50:38 +0900

    [UPDATE] Image File Name env 파일 수정

--- src/app/[locale]/main/_constant/our-business.ts
+++ src/app/[locale]/main/_constant/our-business.ts
@@ -11,7 +11,7 @@ export const OUR_BUSINESS_CONFIG = [
     title: "SAVE-E",
     description: "최소한의 고객 정보를 활용한 \n빅데이터 분석 기반 \n 건물 에너지 절감 솔루션",
     tags: ["#B2B", "#B2G", "#자동레포팅"],
-    image: "/images/main/SaveE.png",
+    image: "/images/main/saveE.png",
   },
   {
     id: 2,
@@ -19,20 +19,20 @@ export const OUR_BUSINESS_CONFIG = [
     description:
       "GIS 기반 시뮬레이션과 AI 기술을 활용해 Energy Retrofit의 최적의 대안을 도출합니다.",
     tags: ["#B2B", "#B2G", "#그린리모델링"],
-    image: "/images/main/GreenPlanner.png",
+    image: "/images/main/greenPlanner.png",
   },
   {
     id: 3,
     title: "그린플래너 APP",
     description: "프롭테크와 리모델링 예측보고서를 결합한 리모델링 원스톱 플랫폼",
     tags: ["#B2B", "#B2C", "#자동레포팅"],
-    image: "/images/main/GreenPlannerApp.png",
+    image: "/images/main/greenPlannerApp.png",
   },
   {
     id: 4,
     title: "RE:park",
     description: "시민·관리자·보수업체를 위한\n 공원시설물 관리시스템",
     tags: ["#B2B", "#B2G", "#GIS"],
-    image: "/images/main/RePark.png",
+    image: "/images/main/rePark.png",
   },
 ];
